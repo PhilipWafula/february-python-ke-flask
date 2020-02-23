f3346e9f91ed54f94e7010ba4b4622ffeade3bd5
Philip Wafula <wanga.philip.devwork@gmail.com>
2020-02-23 11:42:00 +0300

    Refactored import system to add project parent directory to system path

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd app || exit
-
-pip install -r requirements.txt
-
-cd ../tests || exit
-
-pip install -r requirements.txt
--- tests/unit/models/test_user.py
+++
@@ -1,5 +0,0 @@
-def test_create_user(create_user):
-    user = create_user()
-
-    assert user.first_name == 'Test'
-    assert user.last_name == 'User'
